056c32801ce531f6c166438768567c4cfb246a6c
JilsinaRosh <47523555+JilsinaRosh@users.noreply.github.com>
2019-02-12 12:34:26 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.03 
+MySQL - 5.1.32-community : Database - job
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`job` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `job`;
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `lid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`lid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+LOCK TABLES `login` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `signup` */
+
+DROP TABLE IF EXISTS `signup`;
+
+CREATE TABLE `signup` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `dob` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `signup` */
+
+LOCK TABLES `signup` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
